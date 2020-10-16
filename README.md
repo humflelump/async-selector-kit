d5e466b392f2b6d077887dc64280c3021544a084
Jesse Wolgamott <jesse_wolgamott@eogresources.com>
2020-10-16 09:52:42 -0500

    Initial V1 of docs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Async-Selector-Kit Documentation
+
+Created with Gastby
+
+## Getting Started
+
+1. git clone
+2. yarn
+3. `yarn develop`
+
+## Publishing
+
+** Still working on this \***
+
+## Created With
+
+Smooth-Docs
---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "async-selector-kit",
+        description: "Redux Async Selector Framework",
+        siteUrl: "https://example.com",
+      },
+    },
+  ],
+};
